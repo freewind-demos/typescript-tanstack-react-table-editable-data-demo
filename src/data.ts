a2e38a442159b31a04c646868fb370cbccec6a7a
Freewind <nowindlee@gmail.com>
2020-01-07 16:07:18 +0800

    typescript-react-table-demo

---
+++ src/data.ts
@@ -0,0 +1,13 @@
+export type User = {
+  firstName: string,
+  lastName: string,
+  age: string,
+  visits: string,
+}
+
+export const data: User [] = [
+  {firstName: 'aaa', lastName: 'sss', age: 'eee', visits: 'sdfsf'},
+  {firstName: 'aaa', lastName: 'sss', age: 'eee', visits: 'sdfsf'},
+  {firstName: 'aaa', lastName: 'sss', age: 'eee', visits: 'sdfsf'},
+];
+
