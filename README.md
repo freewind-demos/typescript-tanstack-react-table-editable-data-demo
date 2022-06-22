de6f983f6ee9f0a75bfbdc6aa698257fb9fe994e
Freewind <nowindlee@gmail.com>
2022-06-22 20:33:46 +0800

    typescript-tanstack-react-table-editable-data-demo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-TypeScript React Table Column Visibility Demo
+TypeScript React Table Editable Data Demo
 ===========================================
 
 ```
--- src/main.tsx
+++ src/main.tsx
@@ -1,193 +1,320 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+//
 import './index.css'
 
+//
 import {
   createTable,
-  getCoreRowModel,
+  Column,
+  TableInstance,
+  ColumnDef,
   useTableInstance,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
 } from '@tanstack/react-table'
+import { makeData, Person } from './makeData'
 
-type Person = {
-  firstName: string
-  lastName: string
-  age: number
-  visits: number
-  status: string
-  progress: number
+let table = createTable()
+  .setRowType<Person>()
+  // In addition to our row type, we can also tell our table about a custom "updateData" method we will provide it
+  .setTableMetaType<{
+    updateData: (rowIndex: number, columnId: string, value: unknown) => void
+  }>()
+
+// Get our table generics
+type MyTableGenerics = typeof table.generics
+
+// Give our default column cell renderer editing superpowers!
+const defaultColumn: Partial<ColumnDef<MyTableGenerics>> = {
+  cell: ({ getValue, row: { index }, column: { id }, instance }) => {
+    const initialValue = getValue()
+    // We need to keep and update the state of the cell normally
+    const [value, setValue] = React.useState(initialValue)
+
+    // When the input is blurred, we'll call our table meta's updateData function
+    const onBlur = () => {
+      instance.options.meta?.updateData(index, id, value)
+    }
+
+    // If the initialValue is changed external, sync it up with our state
+    React.useEffect(() => {
+      setValue(initialValue)
+    }, [initialValue])
+
+    return (
+      <input
+        value={value as string}
+        onChange={e => setValue(e.target.value)}
+        onBlur={onBlur}
+      />
+    )
+  },
 }
 
-const defaultData: Person[] = [
-  {
-    firstName: 'tanner',
-    lastName: 'linsley',
-    age: 24,
-    visits: 100,
-    status: 'In Relationship',
-    progress: 50,
-  },
-  {
-    firstName: 'tandy',
-    lastName: 'miller',
-    age: 40,
-    visits: 40,
-    status: 'Single',
-    progress: 80,
-  },
-  {
-    firstName: 'joe',
-    lastName: 'dirte',
-    age: 45,
-    visits: 20,
-    status: 'Complicated',
-    progress: 10,
-  },
-]
+function useSkipper() {
+  const shouldSkipRef = React.useRef(true)
+  const shouldSkip = shouldSkipRef.current
 
-let table = createTable().setRowType<Person>()
+  // Wrap a function with this to skip a pagination reset temporarily
+  const skip = React.useCallback(() => {
+    shouldSkipRef.current = false
+  }, [])
 
-const defaultColumns = [
-  table.createGroup({
-    header: 'Name',
-    footer: props => props.column.id,
-    columns: [
-      table.createDataColumn('firstName', {
-        cell: info => info.getValue(),
-        footer: props => props.column.id,
-      }),
-      table.createDataColumn(row => row.lastName, {
-        id: 'lastName',
-        cell: info => info.getValue(),
-        header: () => <span>Last Name</span>,
-        footer: props => props.column.id,
-      }),
-    ],
-  }),
-  table.createGroup({
-    header: 'Info',
-    footer: props => props.column.id,
-    columns: [
-      table.createDataColumn('age', {
-        header: () => 'Age',
-        footer: props => props.column.id,
-      }),
+  React.useEffect(() => {
+    shouldSkipRef.current = true
+  })
+
+  return [shouldSkip, skip] as const
+}
+
+function App() {
+  const rerender = React.useReducer(() => ({}), {})[1]
+
+  const columns = React.useMemo(
+    () => [
       table.createGroup({
-        header: 'More Info',
+        header: 'Name',
+        footer: props => props.column.id,
         columns: [
-          table.createDataColumn('visits', {
-            header: () => <span>Visits</span>,
+          table.createDataColumn('firstName', {
             footer: props => props.column.id,
           }),
-          table.createDataColumn('status', {
-            header: 'Status',
-            footer: props => props.column.id,
-          }),
-          table.createDataColumn('progress', {
-            header: 'Profile Progress',
+          table.createDataColumn(row => row.lastName, {
+            id: 'lastName',
+            header: () => <span>Last Name</span>,
             footer: props => props.column.id,
           }),
         ],
       }),
+      table.createGroup({
+        header: 'Info',
+        footer: props => props.column.id,
+        columns: [
+          table.createDataColumn('age', {
+            header: () => 'Age',
+            footer: props => props.column.id,
+          }),
+          table.createGroup({
+            header: 'More Info',
+            columns: [
+              table.createDataColumn('visits', {
+                header: () => <span>Visits</span>,
+                footer: props => props.column.id,
+              }),
+              table.createDataColumn('status', {
+                header: 'Status',
+                footer: props => props.column.id,
+              }),
+              table.createDataColumn('progress', {
+                header: 'Profile Progress',
+                footer: props => props.column.id,
+              }),
+            ],
+          }),
+        ],
+      }),
     ],
-  }),
-]
+    []
+  )
 
-function App() {
-  const [data, setData] = React.useState(() => [...defaultData])
-  const [columns] = React.useState<typeof defaultColumns>(() => [
-    ...defaultColumns,
-  ])
-  const [columnVisibility, setColumnVisibility] = React.useState({})
+  const [data, setData] = React.useState(() => makeData(1000))
+  const refreshData = () => setData(() => makeData(1000))
 
-  const rerender = React.useReducer(() => ({}), {})[1]
+  const [autoResetPageIndex, skipAutoResetPageIndex] = useSkipper()
 
   const instance = useTableInstance(table, {
     data,
     columns,
-    state: {
-      columnVisibility,
-    },
-    onColumnVisibilityChange: setColumnVisibility,
+    defaultColumn,
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    autoResetPageIndex,
+    // Provide our updateData function to our table meta
+    meta: {
+      updateData: (rowIndex, columnId, value) => {
+        // Skip age index reset until after next rerender
+        skipAutoResetPageIndex()
+        setData(old =>
+          old.map((row, index) => {
+            if (index === rowIndex) {
+              return {
+                ...old[rowIndex]!,
+                [columnId]: value,
+              }
+            }
+            return row
+          })
+        )
+      },
+    },
     debugTable: true,
-    debugHeaders: true,
-    debugColumns: true,
   })
 
   return (
     <div className="p-2">
-      <div className="inline-block border border-black shadow rounded">
-        <div className="px-1 border-b border-black">
-          <label>
-            <input
-              {...{
-                type: 'checkbox',
-                checked: instance.getIsAllColumnsVisible(),
-                onChange: instance.getToggleAllColumnsVisibilityHandler(),
-              }}
-            />{' '}
-            Toggle All
-          </label>
-        </div>
-        {instance.getAllLeafColumns().map(column => {
-          return (
-            <div key={column.id} className="px-1">
-              <label>
-                <input
-                  {...{
-                    type: 'checkbox',
-                    checked: column.getIsVisible(),
-                    onChange: column.getToggleVisibilityHandler(),
-                  }}
-                />{' '}
-                {column.id}
-              </label>
-            </div>
-          )
-        })}
-      </div>
-      <div className="h-4" />
+      <div className="h-2" />
       <table>
         <thead>
         {instance.getHeaderGroups().map(headerGroup => (
           <tr key={headerGroup.id}>
-            {headerGroup.headers.map(header => (
-              <th key={header.id} colSpan={header.colSpan}>
-                {header.isPlaceholder ? null : header.renderHeader()}
-              </th>
-            ))}
+            {headerGroup.headers.map(header => {
+              return (
+                <th key={header.id} colSpan={header.colSpan}>
+                  {header.isPlaceholder ? null : (
+                    <div>
+                      {header.renderHeader()}
+                      {header.column.getCanFilter() ? (
+                        <div>
+                          <Filter
+                            column={header.column}
+                            instance={instance}
+                          />
+                        </div>
+                      ) : null}
+                    </div>
+                  )}
+                </th>
+              )
+            })}
           </tr>
         ))}
         </thead>
         <tbody>
-        {instance.getRowModel().rows.map(row => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map(cell => (
-              <td key={cell.id}>{cell.renderCell()}</td>
-            ))}
-          </tr>
-        ))}
+        {instance.getRowModel().rows.map(row => {
+          return (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => {
+                return <td key={cell.id}>{cell.renderCell()}</td>
+              })}
+            </tr>
+          )
+        })}
         </tbody>
-        <tfoot>
-        {instance.getFooterGroups().map(footerGroup => (
-          <tr key={footerGroup.id}>
-            {footerGroup.headers.map(header => (
-              <th key={header.id} colSpan={header.colSpan}>
-                {header.isPlaceholder ? null : header.renderFooter()}
-              </th>
-            ))}
-          </tr>
-        ))}
-        </tfoot>
       </table>
-      <div className="h-4" />
-      <button onClick={() => rerender()} className="border p-2">
-        Rerender
-      </button>
-      <div className="h-4" />
-      <pre>{JSON.stringify(instance.getState().columnVisibility, null, 2)}</pre>
+      <div className="h-2" />
+      <div className="flex items-center gap-2">
+        <button
+          className="border rounded p-1"
+          onClick={() => instance.setPageIndex(0)}
+          disabled={!instance.getCanPreviousPage()}
+        >
+          {'<<'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => instance.previousPage()}
+          disabled={!instance.getCanPreviousPage()}
+        >
+          {'<'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => instance.nextPage()}
+          disabled={!instance.getCanNextPage()}
+        >
+          {'>'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => instance.setPageIndex(instance.getPageCount() - 1)}
+          disabled={!instance.getCanNextPage()}
+        >
+          {'>>'}
+        </button>
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {instance.getState().pagination.pageIndex + 1} of{' '}
+            {instance.getPageCount()}
+          </strong>
+        </span>
+        <span className="flex items-center gap-1">
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={instance.getState().pagination.pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              instance.setPageIndex(page)
+            }}
+            className="border p-1 rounded w-16"
+          />
+        </span>
+        <select
+          value={instance.getState().pagination.pageSize}
+          onChange={e => {
+            instance.setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>{instance.getRowModel().rows.length} Rows</div>
+      <div>
+        <button onClick={() => rerender()}>Force Rerender</button>
+      </div>
+      <div>
+        <button onClick={() => refreshData()}>Refresh Data</button>
+      </div>
     </div>
+  )
+}
+function Filter({
+                  column,
+                  instance,
+                }: {
+  column: Column<any>
+  instance: TableInstance<any>
+}) {
+  const firstValue = instance
+    .getPreFilteredRowModel()
+    .flatRows[0]?.getValue(column.id)
+
+  const columnFilterValue = column.getFilterValue()
+
+  return typeof firstValue === 'number' ? (
+    <div className="flex space-x-2">
+      <input
+        type="number"
+        value={(columnFilterValue as [number, number])?.[0] ?? ''}
+        onChange={e =>
+          column.setFilterValue((old: [number, number]) => [
+            e.target.value,
+            old?.[1],
+          ])
+        }
+        placeholder={`Min`}
+        className="w-24 border shadow rounded"
+      />
+      <input
+        type="number"
+        value={(columnFilterValue as [number, number])?.[1] ?? ''}
+        onChange={e =>
+          column.setFilterValue((old: [number, number]) => [
+            old?.[0],
+            e.target.value,
+          ])
+        }
+        placeholder={`Max`}
+        className="w-24 border shadow rounded"
+      />
+    </div>
+  ) : (
+    <input
+      type="text"
+      value={(columnFilterValue ?? '') as string}
+      onChange={e => column.setFilterValue(e.target.value)}
+      placeholder={`Search...`}
+      className="w-36 border shadow rounded"
+    />
   )
 }
 
